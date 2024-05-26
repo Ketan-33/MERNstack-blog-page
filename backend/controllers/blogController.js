b9711e8df0a02d1785a26df7674a558b99c1a5c3
ketan <ketanpardhi04@gmail.com>
2024-05-26 16:14:23 +0530

    delete blog functionality

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -120,3 +120,18 @@ if (
   });
 });
 
+ export const deleteBlog = catchAsyncErrors(async (req, res, next) => {
+   const { id } = req.params;
+   const blog = await Blog.findById(id);
+   if (!blog) {
+     return next(new ErrorHandler("Blog not found!", 404));
+   }
+   await blog.deleteOne();
+   res.status(200).json({
+     success: true,
+     message: "Blog deleted!",
+   });
+ });
+
+
+
