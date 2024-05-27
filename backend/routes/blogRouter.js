ccb08bc974a88a453defc599f7f2c28e2ce2a2b8
ketan <ketanpardhi04@gmail.com>
2024-05-27 14:17:38 +0530

    update blog functionality

--- backend/routes/blogRouter.js
+++ backend/routes/blogRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { blogPost,deleteBlog,getAllBlogs,getMyBlogs,getSingleBlog,} from "../controllers/blogController.js";
+import { blogPost,deleteBlog,getAllBlogs,getMyBlogs,getSingleBlog,updateBlog} from "../controllers/blogController.js";
 import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -9,6 +9,5 @@ router.delete("/delete/:id", isAuthenticated, isAuthorized("Author"), deleteBlog
 router.get("/all", getAllBlogs);
 router.get("/singleblog/:id", isAuthenticated, getSingleBlog);
 router.get("/myblogs", isAuthenticated, isAuthorized("Author"), getMyBlogs);
-
-
+router.put("/update/:id", isAuthenticated, isAuthorized("Author"), updateBlog);
 export default router;
