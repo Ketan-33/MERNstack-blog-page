ee146587fdba2eb3f217cf7bb6c1ae60bf3079a4
ketan <ketanpardhi04@gmail.com>
2024-05-31 17:30:17 +0530

    configuring pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blogs from "./components/pages/Blogs";
+import SingleBlog from "./components/pages/SingleBlog";
+import Dashboard from "./components/pages/Dashboard";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import AllAuthors from "./components/pages/AllAuthors";
+import UpdateBlog from "./components/pages/UpdateBlog";
 
 const App = () => {
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blog/:id" element={<SingleBlog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/authors" element={<AllAuthors />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/blog/update/:id" element={<UpdateBlog />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
