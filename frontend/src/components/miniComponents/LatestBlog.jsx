7b6c23b03d264d910d32f2b89860cd40e29b8117
ketan <ketanpardhi04@gmail.com>
2024-06-01 13:24:24 +0530

    mini components- Latest Blogs

--- frontend/src/components/miniComponents/LatestBlog.jsx
+++ frontend/src/components/miniComponents/LatestBlog.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function LatestBlog() {
+const LatestBlog = ({ heading, newClass, blogs }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section
+      className={
+        newClass && newClass.length > 0 ? "dashboard-blogs blogs" : "blogs"
+      }
+    >
+      <h3>{heading}</h3>
+      <div className="container">
+        {blogs &&
+          blogs.map((element) => {
+            return (
+              <Link
+                to={`/blog/${element._id}`}
+                className="card"
+                key={element._id}
+              >
+                <img src={element.mainImage.url} alt="blog" />
+                <span className="category">{element.category}</span>
+                <h4>{element.title}</h4>
+                <div className="writer_section">
+                  <div className="author">
+                    <img src={element.authorAvatar} alt="author_avatar" />
+                    <p>{element.authorName}</p>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
+    </section>
+  );
+};
 
-export default LatestBlog
+export default LatestBlog;
