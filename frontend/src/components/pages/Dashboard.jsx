9e117f787ad2f2b175f88830cfb7b6fe8cd5a095
ketan <ketanpardhi04@gmail.com>
2024-06-05 00:58:42 +0530

    dashboard and sidebar

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import React, { useContext, useState } from "react";
+import SideBar from "../layout/SideBar";
+import MyBlog from "../miniComponents/MyBlog";
+import MyProfile from "../miniComponents/MyProfile";
+import CreateBlog from "../miniComponents/CreateBlog";
+import Chart from "../miniComponents/Chart";
+import { Context } from "../../main";
+import { Navigate } from "react-router-dom";
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  const [component, setComponent] = useState("MyBlog");
+  const { mode, isAuthenticated } = useContext(Context);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <section
+      className={mode === "dark" ? "dark-bg dashboard" : "light-bg dashboard"}
+    >
+      <SideBar component={component} setComponent={setComponent} />
+      {component === "My Profile" ? (
+        <MyProfile />
+      ) : component === "Create Blog" ? (
+        <CreateBlog />
+      ) : component === "Analytics" ? (
+        <Chart />
+      ) : (
+        <MyBlog />
+      )}
+    </section>
+  );
 };
 
 export default Dashboard;
